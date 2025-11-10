1f1e8b08ae8f76e863ff80102c454a41e52380a8
Homework-10 <danilovsansan@gmail.com>
2025-11-10 07:44:03 +0300

    сохранил пакет exeption  в git

---
+++ src/org/skypro/skyshop/exeption/IllegalDiscountPriceArgumentException.java
@@ -0,0 +1,6 @@
+package org.skypro.skyshop.exeption;
+
+public class IllegalDiscountPriceArgumentException extends IllegalArgumentException {
+    public void IllegalArgumentException(String s) {
+    }
+}
